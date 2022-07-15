b37cc91ed7aae7768634877dbe244022d67be121
secolema <seanclmn1@gmail.com>
2022-07-14 23:47:49 -0400

    quiz game props

--- src/components/QuizGame.tsx
+++ src/components/QuizGame.tsx
@@ -2,21 +2,24 @@ import React from 'react'
 import {useParams} from 'react-router-dom'
 import QuizAnswerButton from './QuizAnswerButton'
 
-function QuizGame() {
+interface QuizGameProps {
+  question: string
+  answer: string
+  answers: string[]
+}
+
+function QuizGame({question,answer,answers}:QuizGameProps) {
   let {levelquiz} = useParams()
   console.log(levelquiz)
 
+  const correct = () => 
+
+
   return (
     <div className="mx-auto w-[70%]">
-
-      
-
-
+      <p>{question}</p>
       <div className="grid grid-cols-2 gap-4">
-        <QuizAnswerButton text="test"/>
-        <QuizAnswerButton text="test"/>
-        <QuizAnswerButton text="test"/>
-        <QuizAnswerButton text="test"/>
+        {answers.map(answer=> <QuizAnswerButton key={answers.indexOf(answer)} text={answer}/>)}
       </div>
     </div>
  )
