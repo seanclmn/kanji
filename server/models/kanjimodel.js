1f04f6752a6f869df9d4339d02d554fc2d532b5e
secolema <seanclmn1@gmail.com>
2022-09-05 19:39:26 -0400

    Added get questions route

--- server/models/kanjimodel.js
+++ server/models/kanjimodel.js
@@ -2,24 +2,24 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 
-// const KanjiSchema = new mongoose.Schema({
-// 	strokes: Number,
-// 	grade: Number,
-// 	freq: Number,
-// 	jlpt_old: Number,
-// 	jlpt_new: Number,
-// 	meanings: [String],
-// 	readings_on: [String],
-// 	readings_kun: [String],
-// 	wk_level: Number,
-// 	wk_meanings: [String],
-// 	wk_readings_on: [String],
-// 	wk_readings_kun: [String],
-// 	wk_radicals: [String],
-// 	kanji: String
-// })
+const KanjiSchema = new mongoose.Schema({
+	strokes: Number,
+	grade: Number,
+	freq: Number,
+	jlpt_old: Number,
+	jlpt_new: Number,
+	meanings: [String],
+	readings_on: [String],
+	readings_kun: [String],
+	wk_level: Number,
+	wk_meanings: [String],
+	wk_readings_on: [String],
+	wk_readings_kun: [String],
+	wk_radicals: [String],
+	kanji: String
+})
 
-const KanjiSchema = new Schema({},{ strict: false})
+// const KanjiSchema = new Schema({},{ strict: false})
 // const Kanji = mongoose.model('Kanji',KanjiSchema,'kanji')
 // module.exports = {Kanji}
 
--- server/routes/api/kanji.js
+++ server/routes/api/kanji.js
@@ -5,11 +5,33 @@ const Kanji = require('../../models/kanjimodel')
 
 router.get('/test', (req,res) => res.send('this is a test'))
 
-router.get('/', (req,res) => {
+router.get('/fullkanjilist', (req,res) => {
 	Kanji.find()
 		.then(kanji => res.json(kanji))
 		.then(res => console.log(res.data))
 		.catch(err => res.status(404).json({nokanjifound: "No kanji were found"}))
+})
+
+router.get('/fullkanjilist/grade/:grade', (req,res)=>{
+	Kanji.find({"grade": req.params.grade})
+		.then(kanji => res.json(kanji))
+		.catch(err => console.log(err.message))
+})
+
+router.get('/kanjiquestions/:grade/:length',(req,res)=>{
+	const createIds = (length) => {
+		const questionsLength = req.params.length
+		var indices = []
+		for(let i=0;i<questionsLength;i++){
+			indices.push(Math.floor(Math.random()*length))
+		}
+		return(indices)
+	}
+	
+	Kanji.find({"grade": req.params.grade})
+		.then(kanji => res.json(createIds(kanji.length).map(index=>kanji[index])))
+		.catch(err => console.log(err.message))
+
 })
 
 module.exports = router 
