064202cb4c14c35c863850ceea9764de6a4fe926
secolema <seanclmn1@gmail.com>
2022-09-09 23:16:41 -0400

    added env

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// const config = require('config');
+// const db = config.get('mongoURI');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
+  console.log( `${process.env.MONGO_URI}`)
   try {
     await mongoose.connect(
-      db,
+      process.env.MONGO_URI,
       {
         useNewUrlParser: true
       }
