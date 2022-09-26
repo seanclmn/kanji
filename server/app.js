5b22c25b86e710f2cf5b9890ccfadb81db42c2c4
Sean Coleman <78179581+seanclmn@users.noreply.github.com>
2022-09-26 00:20:18 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,12 +10,12 @@ const kanji = require('./routes/api/kanji')
 app.use('/api/kanji',kanji)
 
 
-// //Serve static files
+//Serve static files
 
-// app.use(express.static(__dirname+'/build'))
-// app.get('*', function(req, res) {
-//   res.sendFile('index.html', {root: __dirname+'/build'});
-// });
+app.use(express.static(__dirname+'/build'))
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: __dirname+'/build'});
+});
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
