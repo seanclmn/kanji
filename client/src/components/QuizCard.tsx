1c19619caf242527c2cbcec2c1565a9ad1470202
secolema <seanclmn1@gmail.com>
2022-10-04 21:34:32 -0400

    Adding images and colors to quiz cards

--- client/src/components/QuizCard.tsx
+++ client/src/components/QuizCard.tsx
@@ -1,4 +1,4 @@
-import { Text, Card, RingProgress } from '@mantine/core';
+import { Text, Card, RingProgress, Title } from '@mantine/core';
 import { Link } from 'react-router-dom'
 
 
@@ -9,6 +9,14 @@ interface QuizCardProps {
 	link: string
 }
 
+const colorMap = [
+	"bg-gradient-to-r from-blue-500 to-cyan-300",
+	"bg-gradient-to-r from-indigo-500 to-pink-300",
+	"bg-gradient-to-r from-red-500 to-rose-300",
+	"bg-gradient-to-r from-cyan-500 to-blue-300",
+	"bg-gradient-to-r from-lime-500 to-green-200",
+	"bg-gradient-to-r from-orange-600 to-rose-200",
+	"bg-rose-200","bg-rose-200","bg-rose-200","bg-pink-300"]
 
 const QuizCard = ({level, progress, color, link}: QuizCardProps) =>{
 
@@ -16,29 +24,37 @@ const QuizCard = ({level, progress, color, link}: QuizCardProps) =>{
 		<Link to={link} rel="prefetch" className='no-underline'>
 			<Card 
 				withBorder 
-				p="xl" 
+				p="none" 
 				radius="md"
-				className='mb-4 flex flex-row justify-left px-5 py-0 h-[200px]'
+				className='mx-auto mb-4 flex flex-row pl-0 pr-0 h-[200px] justify-between max-w-[1000px]'
 				>
-				<div
-					className="w-[150px] text-center"
-					>
-					<Text>	
-					GRADE {level}
-					</Text>
-					<img className="h-[100%] "src={process.env.PUBLIC_URL+`/grade_${level}.png`} alt={`/grade_${level}.png`} />
+				<div className={"flex flex-row justify-around w-[50%] items-center py-5 "+colorMap[eval(level)-1]}>
+					<p className='bg-transparent text-[120px] leading-none my-0 text-white bottom-0'>
+						L{level}
+					</p>
+
+					<Card className='flex flex-col items-center bg-transparent'>
+						<Text size="lg" color="white">HIGH SCORE</Text>
+						<RingProgress
+							sections={[{ value: progress, color: "white" },{ value: 100-progress, color: "#dedede60" }]}
+							label={
+								<Text color="white" weight={700} align="center" size="xl">
+									{progress}%
+								</Text>
+							}
+						/>
+
+					</Card>
 				</div>
-				<div className="flex flex-col items-center w-[150px]">
-					<Text>HIGH SCORE</Text>
-					<RingProgress
-						sections={[{ value: progress, color: color }]}
-						label={
-							<Text color={color} weight={700} align="center" size="xl">
-								{progress}%
-							</Text>
-						}
+				<img 
+					className="
+						w-[50%] 
+						h-[100%] 
+						object-cover
+						"
+					src={process.env.PUBLIC_URL+`/grade_${level}.png`} 
+					alt={`/grade_${level}.png`} 
 					/>
-				</div>
 			</Card>
 		</Link>
 
--- client/src/pages/QuizzesPage.tsx
+++ client/src/pages/QuizzesPage.tsx
@@ -6,9 +6,16 @@ import { useSelector } from 'react-redux';
 import { getUserData, changeScore } from '../features/kanji/kanjiSlice';
 import { useAppDispatch } from '../app/hooks';
 
+const colorMap = [
+	"bg-gradient-to-r from-blue-500 to-cyan-300",
+	"bg-gradient-to-r from-indigo-500 to-pink-300",
+	"bg-gradient-to-r from-red-500 to-rose-300",
+	"bg-gradient-to-r from-cyan-500 to-blue-300",
+	"bg-rose-200","bg-rose-200","bg-rose-200","bg-rose-200"]
+
 const QuizesPage = () => {
 	const dataStore = useSelector(getUserData)
-	const Quizzes = dataStore.userData.userData.kanjiTopScores.map((score,kanjiLevel)=>({color: '#68b5e8', progress: score, level: kanjiLevel+1 , id: kanjiLevel+1}))
+	const Quizzes = dataStore.userData.userData.kanjiTopScores.map((score,kanjiLevel)=>({color: colorMap[kanjiLevel], progress: score, level: kanjiLevel+1 , id: kanjiLevel+1}))
 
 	return(
 		<div className="pt-5">
